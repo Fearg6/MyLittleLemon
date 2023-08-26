d61f9e031d26f7fbf413a8eab615803a3af6461c
Fearghal <Fearghal.mcmorrow@gmail.com>
2023-08-26 21:09:36 +0100

    Added sematic HTML and og meta

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import "./App.css";
+import { Helmet } from "react-helmet";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <Helmet>
+        <meta property="og:title" content="Little Lemon" />
+        <meta
+          property="og:description"
+          content="Little Lemon is a resturant."
+        />
+        <meta property="og:image" content="little_lemon_logo.png" />
+        <meta property="og:url" content="https://little-lemon.com" />
+      </Helmet>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
